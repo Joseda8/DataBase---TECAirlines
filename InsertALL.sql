8aba584c03316df16dbb344500fc7d4b64b0d4a4
Joseda8 <jd.montoya.s8@gmail.com>
2019-04-29 14:33:08 -0600

    Acoplamiento al cambio en el tipo de dato de is_student

--- InsertALL.sql
+++ InsertALL.sql
@@ -84,15 +84,15 @@ INSERT INTO ADMIN VALUES ('Joseph Rodríguez Vargas', '13243546', 'j.rv@gmail.co
 INSERT INTO ADMIN VALUES ('Marck Castro Ramírez', '14253647', 'm.cr@gmail.com', 'Loladmin', 'chocolover', 'Supervisor')
 INSERT INTO ADMIN VALUES ('Charles Rojas Montero', '10108080', 'c.rm@gmail.com', 'henkidama', 'm31_000h@', 'Supervisor')
 
-INSERT INTO CUSTOMER VALUES ('José Daniel Montoya Salazar', '88888888', 'jd.m@gmail.com', 'Joseda8', 'green', True)
-INSERT INTO CUSTOMER VALUES ('José Núñez Valverde', '12345678', 'jc.n@gmail.com', 'Joselml', 'ice', True)
-INSERT INTO CUSTOMER VALUES ('Sebastián Mora', '87654321', 's.m@gmail.com', 'ElectroTECBoy', 'sunday', True)
-INSERT INTO CUSTOMER VALUES ('Marcelo Sánchez Solano', '66666666', 'm.ss@gmail.com', 'AzucarAmargo', 'lol', True)
+INSERT INTO CUSTOMER VALUES ('José Daniel Montoya Salazar', '88888888', 'jd.m@gmail.com', 'Joseda8', 'green', 1)
+INSERT INTO CUSTOMER VALUES ('José Núñez Valverde', '12345678', 'jc.n@gmail.com', 'Joselml', 'ice', 1)
+INSERT INTO CUSTOMER VALUES ('Sebastián Mora', '87654321', 's.m@gmail.com', 'ElectroTECBoy', 'sunday', 1)
+INSERT INTO CUSTOMER VALUES ('Marcelo Sánchez Solano', '66666666', 'm.ss@gmail.com', 'AzucarAmargo', 'lol', 1)
 
-INSERT INTO CUSTOMER VALUES ('Kurt D', '12121515', 'kurt.d@gmail.com', 'Nevermind', 'smellslikeareference', False)
-INSERT INTO CUSTOMER VALUES ('Billie Joe A', '87909090', 'one_dollar_bill@gmail.com', 'Dookie', 'basketcase', False)
-INSERT INTO CUSTOMER VALUES ('Jane Doe', '00000000', 'jane.d@gmail.com', 'SomeGirl', 'somepassword', False)
-INSERT INTO CUSTOMER VALUES ('John Doe', '00000001', 'john.d@gmail.com', 'SomeMen', 'somepassword', False)
+INSERT INTO CUSTOMER VALUES ('Kurt D', '12121515', 'kurt.d@gmail.com', 'Nevermind', 'smellslikeareference', 0)
+INSERT INTO CUSTOMER VALUES ('Billie Joe A', '87909090', 'one_dollar_bill@gmail.com', 'Dookie', 'basketcase', 0)
+INSERT INTO CUSTOMER VALUES ('Jane Doe', '00000000', 'jane.d@gmail.com', 'SomeGirl', 'somepassword', 0)
+INSERT INTO CUSTOMER VALUES ('John Doe', '00000001', 'john.d@gmail.com', 'SomeMen', 'somepassword', 0)
 
 INSERT INTO STUDENTS VALUES (1, 'Joseda8', 'UNED', 0)
 INSERT INTO STUDENTS VALUES (2, 'Joselml', 'ITCR', 0)
