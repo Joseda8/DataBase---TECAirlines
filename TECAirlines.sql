08030f3b818dd42b5fc9af8be29fa6bc1a450f53
Sebastián Mora <sebas.smr@hotmail.com>
2019-05-04 14:05:27 -0600

    Update TECAirlines.sql

--- TECAirlines.sql
+++ TECAirlines.sql
@@ -1,107 +1,99 @@
-CREATE TABLE UNIVERSITY(
-	uni_name varchar(60) PRIMARY KEY
-)
+INSERT INTO UNIVERSITY VALUES ('ITCR')
+INSERT INTO UNIVERSITY VALUES ('UCR')
+INSERT INTO UNIVERSITY VALUES ('UNA')
+INSERT INTO UNIVERSITY VALUES ('UNED')
+INSERT INTO UNIVERSITY VALUES ('UTN')
+INSERT INTO UNIVERSITY VALUES ('INDEPENDENT')
 
-CREATE TABLE AIRPORT(
-	ap_name varchar(60) PRIMARY KEY,
-	ap_short_name varchar(10) NOT NULL UNIQUE,
-	iata_code char(3) NOT NULL,
-	country_name varchar(30) NOT NULL
-)
+INSERT INTO AIRPORT VALUES ('Annaba Les Salines', 'AnnLesSal', 'AAE', 'Algeria')
+INSERT INTO AIRPORT VALUES ('Aalborg', 'Aal', 'AAL', 'Denmark')
+INSERT INTO AIRPORT VALUES ('Mala Mala', 'Mal', 'AAM', 'South Africa')
+INSERT INTO AIRPORT VALUES ('Al Ain', 'AAin', 'AAN', 'United Arab Emirates')
+INSERT INTO AIRPORT VALUES ('Aeropuerto Internacional Juan Santamaría', 'AJS', 'SJO', 'Costa Rica')
+INSERT INTO AIRPORT VALUES ('Anapa', 'Anap', 'AAQ', 'Russia')
+INSERT INTO AIRPORT VALUES ('Altay', 'Alt', 'AAT', 'China')
+INSERT INTO AIRPORT VALUES ('Araxa', 'Arax', 'AAX', 'Brazil')
+INSERT INTO AIRPORT VALUES ('Al Ghaydah', 'AlG', 'AAY', 'Yemen')
+INSERT INTO AIRPORT VALUES ('Albacete Los Llanos', 'AlbLl', 'ABC', 'Spain') 
+INSERT INTO AIRPORT VALUES ('Abadan', 'Abad', 'ABD', 'Iran')
+INSERT INTO AIRPORT VALUES ('Abilene Regional Airport', 'AbiRA', 'ABI', 'United States')
+INSERT INTO AIRPORT VALUES ('Bamaga', 'Bam', 'ABM', 'Australia')
+INSERT INTO AIRPORT VALUES ('Albuquerque International', 'AlbuIn', 'ABQ', 'United States')
+INSERT INTO AIRPORT VALUES ('Aberdeen Municipal', 'AberdMun', 'ABR', 'United States')
+INSERT INTO AIRPORT VALUES ('Abu Simbel', 'AbuSim', 'ABS', 'Egypt')
+INSERT INTO AIRPORT VALUES ('Al Baha Al Aqiq', 'AlBahAq', 'ABT', 'Saudi Arabia')
+INSERT INTO AIRPORT VALUES ('Abuja International', 'AbuIn', 'ABV', 'Nigeria')
+INSERT INTO AIRPORT VALUES ('Aberdeen Dyce', 'AberDy', 'ABZ', 'United Kingdom')
+INSERT INTO AIRPORT VALUES ('Acapulco Juan Alvarez International', 'AcaIn', 'ACA', 'Mexico')
+INSERT INTO AIRPORT VALUES ('Accra Kotoka', 'AccKot', 'ACC', 'Ghana')
+INSERT INTO AIRPORT VALUES ('Altenrhein', 'Altenr', 'ACH', 'Switzerland')
+INSERT INTO AIRPORT VALUES ('Adiyaman', 'Adiy', 'ADF', 'Turkey')
+INSERT INTO AIRPORT VALUES ('San Andres Island', 'SanAndIs', 'ADZ', 'Colombia')
+INSERT INTO AIRPORT VALUES ('Arpt Jorge Newbery', 'BuAirJorNew', 'AEP', 'Argentina')
+INSERT INTO AIRPORT VALUES ('Aalesund Vigra', 'AalVig', 'AES', 'Norway')
+INSERT INTO AIRPORT VALUES ('Agadir Almassira', 'AgaAlm', 'AGA', 'Morocco')
+INSERT INTO AIRPORT VALUES ('Agen La Garenne', 'AgGar', 'AGF', 'France')
+INSERT INTO AIRPORT VALUES ('Helsingborg Angelholm', 'HelAng', 'AGH', 'Sweden')
+INSERT INTO AIRPORT VALUES ('Agra Kheria', 'AgrKhe', 'AGR', 'India')
+INSERT INTO AIRPORT VALUES ('Alghero Fertilia', 'AlgFer', 'AHO', 'Italy')
+INSERT INTO AIRPORT VALUES ('Ahuas', 'Ahu', 'AHS', 'Honduras')
+INSERT INTO AIRPORT VALUES ('Asahikawa', 'Asah', 'AKJ', 'Japan')
+INSERT INTO AIRPORT VALUES ('Akulivik', 'Akul', 'AKV', 'Canada')
+INSERT INTO AIRPORT VALUES ('Aktyubinsk', 'Akty', 'AKX', 'Kazakhstan')
+INSERT INTO AIRPORT VALUES ('Ambon Pattimura', 'AmbPat', 'AMQ', 'Indonesia')
+INSERT INTO AIRPORT VALUES ('Amsterdam Schiphol', 'AmsSch', 'AMS', 'Netherlands')
+INSERT INTO AIRPORT VALUES ('Andahuaylas', 'Andah', 'ANS', 'Peru')
+INSERT INTO AIRPORT VALUES ('Karpathos', 'Karp', 'AOK', 'Greece')
+INSERT INTO AIRPORT VALUES ('Arthurs Town', 'ArthTown', 'ATC', 'Bahamas')
+INSERT INTO AIRPORT VALUES ('Baracoa', 'Barac', 'BCA', 'Cuba')
+INSERT INTO AIRPORT VALUES ('Bhadrapur', 'Bhad', 'BDP', 'Nepal')
+INSERT INTO AIRPORT VALUES ('Belgrade Beograd', 'BelBeo', 'BEG', 'Serbia')
+INSERT INTO AIRPORT VALUES ('Samana International', 'SamIn', 'AZS', 'Dominican Republic')
+INSERT INTO AIRPORT VALUES ('Benghazi Benina Intl', 'BengBenInt', 'BEN', 'Libya')
+INSERT INTO AIRPORT VALUES ('Berlin', 'Ber', 'BER', 'Germany')
+INSERT INTO AIRPORT VALUES ('Aguadilla Borinquen', 'AguadBor', 'BQN', 'Puerto Rico')
+INSERT INTO AIRPORT VALUES ('Hartsfield-Jackson', 'HartJack', 'ATL', 'United States')
+INSERT INTO AIRPORT VALUES ('Aeropuerto Internacional de Los Ángeles', 'LA', 'LAX', 'United States')
+INSERT INTO AIRPORT VALUES ('John F. Kennedy', 'JFK', 'JFK', 'United States')
+INSERT INTO AIRPORT VALUES ('Aeropuerto Internacional de Miami', 'MIA', 'MIA', 'United States')
+INSERT INTO AIRPORT VALUES ('Taba International', 'TabI', 'TCP', 'Egypt')
+INSERT INTO AIRPORT VALUES ('Aeropuerto de Portela', 'Port', 'LIS', 'Portugal')
+INSERT INTO AIRPORT VALUES ('Aeropuerto de Glamoc', 'Glam', 'LQGL', 'Bosnia Herzegovina')
+INSERT INTO AIRPORT VALUES ('Aeropuerto de Dublin', 'DUB', 'DUB', 'Ireland')
+INSERT INTO AIRPORT VALUES ('Aeropuerto de Eindhoven', 'Eind', 'EIN', 'Netherlands')
 
-CREATE TABLE AIRPLANES(
-	model varchar(20) NOT NULL,
-	capacity int NOT NULL,
-	plane_id int PRIMARY KEY,
-	fc_capacity int NOT NULL
-)
+INSERT INTO AIRPLANES VALUES ('XXXX', 0, 0, 0)
+INSERT INTO AIRPLANES VALUES ('A380', 853, 1, 170)
+INSERT INTO AIRPLANES VALUES ('B747', 467, 2, 93)
+INSERT INTO AIRPLANES VALUES ('B777', 400, 3, 80)
+INSERT INTO AIRPLANES VALUES ('A340', 375, 4, 75)
+INSERT INTO AIRPLANES VALUES ('A330', 335, 5, 67)
+INSERT INTO AIRPLANES VALUES ('A380', 853, 6, 170)
+INSERT INTO AIRPLANES VALUES ('B747', 467, 7, 93)
+INSERT INTO AIRPLANES VALUES ('B777', 400, 8, 80)
+INSERT INTO AIRPLANES VALUES ('A340', 375, 9, 75)
+INSERT INTO AIRPLANES VALUES ('A330', 335, 10, 67)
+INSERT INTO AIRPLANES VALUES ('B737', 100, 11, 0)
+INSERT INTO AIRPLANES VALUES ('A320', 220, 12, 44)
+INSERT INTO AIRPLANES VALUES ('A319', 124, 13, 32)
+INSERT INTO AIRPLANES VALUES ('A319', 156, 14, 0)
+INSERT INTO AIRPLANES VALUES ('B737', 100, 15, 0)
+INSERT INTO AIRPLANES VALUES ('A320', 220, 16, 44)
 
-CREATE TABLE ADMIN(
-	full_name varchar(50) NOT NULL,
-	phone_numbr varchar(30) UNIQUE NOT NULL,
-	email varchar(50) UNIQUE NOT NULL,
-	username varchar(20) PRIMARY KEY, 
-	password varchar(30) NOT NULL,
-	role varchar(20) NOT NULL
-)
 
-CREATE TABLE CUSTOMER(
-	full_name varchar(50) NOT NULL,
-	phone_numbr varchar(50) UNIQUE NOT NULL,
-	email varchar(50) UNIQUE NOT NULL,
-	is_student bit NOT NULL,
-	username varchar(20) PRIMARY KEY, 
-	password varchar(50) NOT NULL,
-)
 
-CREATE TABLE STUDENTS(
-	student_id int PRIMARY KEY,
-	username varchar(20) FOREIGN KEY
-	REFERENCES CUSTOMER(username),
-	college_name varchar(60) FOREIGN KEY (college_name) 
-	REFERENCES UNIVERSITY (uni_name),
-	st_miles int NOT NULL,
-)
-
-CREATE TABLE FLIGHT(
-	depart_ap varchar(60) FOREIGN KEY
-	REFERENCES AIRPORT(ap_name) NOT NULL,
-	arrival_ap varchar(60) FOREIGN KEY
-	REFERENCES AIRPORT(ap_name) NOT NULL,
-	capacity int NOT NULL,
-	flight_id varchar(10) PRIMARY KEY,
-	depart_date datetime NOT NULL,
-	plane_id int FOREIGN KEY
-	REFERENCES AIRPLANES(plane_id) NOT NULL,
-	status varchar(20) NOT NULL,
-	normal_price int NOT NULL, 
-	fc_price int NOT NULL, 
-	seats_left int NOT NULL, 
-	fc_seats_left int NOT NULL, 
-	miles_price int NOT NULL
-)
-
-CREATE TABLE PAYMENT_METHOD(
-	payment_id int identity(1,1) PRIMARY KEY,
-	username varchar(20) FOREIGN KEY
-	REFERENCES CUSTOMER(username),
-	card_number varchar(50) NOT NULL,
-	security_code varchar(25) NOT NULL,
-	exp_date date NOT NULL
-)
-
-CREATE TABLE RESERVATION(
-	username varchar(20) FOREIGN KEY
-	REFERENCES CUSTOMER(username),
-	flight_id varchar(10) FOREIGN KEY
-	REFERENCES FLIGHT(flight_id),
-	type varchar(20) NOT NULL,
-	is_first_class bit NOT NULL,
-	people_flying int NOT NULL,
-	total_cost int NOT NULL
-	PRIMARY KEY(username, flight_id)
-)
-
-CREATE TABLE SALE(
-	flight_id varchar(10) FOREIGN KEY 
-	REFERENCES FLIGHT(flight_id),
-	discount int NOT NULL,
-	start_date date NOT NULL,
-	exp_date date NOT NULL,
-	sale_id int identity(1,1) PRIMARY KEY
-)
-
-CREATE TABLE PRE_CHECKING(
-	id_prechecking int identity(1,1) PRIMARY KEY,
-	flight_id varchar(10) FOREIGN KEY 
-	REFERENCES FLIGHT(flight_id),
-	username varchar(20) FOREIGN KEY
-	REFERENCES CUSTOMER(username),
-)
-
-CREATE TABLE PRE_CHECKING_SEATS(
-		id_prechecking_seats int identity(1,1) PRIMARY KEY,
-		id_prechecking int FOREIGN KEY
-		REFERENCES PRE_CHECKING(id_prechecking),
-		seat varchar(10) UNIQUE NOT NULL
-)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('1', 'Al Ain', 'Aeropuerto de Portela', '15/6/2019', 1, 853, 'Active', 700, 1000, 683, 170, 5000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('2', 'Aeropuerto de Eindhoven', 'Aalborg', '15/6/2019', 2, 467, 'Active', 700, 1000, 374, 93, 3500)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('3', 'Mala Mala', 'Aeropuerto de Dublin', '17/6/2019', 3, 400, 'Active', 800, 1200, 320, 80, 5000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('4', 'Aeropuerto Internacional Juan Santamaría', 'Taba International', '18/6/2019', 4, 375, 'Active', 1100, 1500, 300, 75, 8000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('5', 'Aeropuerto Internacional de Miami', 'Anapa', '18/6/2019', 5, 335, 'Active', 800, 1200, 268, 67, 9000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('6', 'Altay', 'Berlin', '20/6/2019', 6, 853, 'Active', 900, 1300, 683, 170, 6000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('7', 'Araxa', 'Aeropuerto de Dublin', '21/6/2019', 7, 467, 'Active', 700, 1000, 374, 93, 5000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('8', 'Al Ghaydah', 'Aeropuerto de Portela', '22/6/2019', 8, 400, 'Active', 850, 1200, 320, 80, 8500)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('9', 'Albacete Los Llanos', 'Bhadrapur', '23/6/2019', 9, 375, 'Active', 1000, 1700, 300, 75, 6000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('10', 'Abadan', 'Baracoa', '25/6/2019', 10, 335, 'Active', 1000, 1400, 268, 67, 10000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('11', 'Aberdeen Municipal', 'Aeropuerto Internacional de Los Ángeles', '25/6/2019', 11, 100, 'Active', 500, 0, 100, 0, 3000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('12', 'Adiyaman', 'Agen La Garenne', '26/6/2019', 12, 220, 'Active', 700, 1000, 176, 44, 5000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('13', 'Andahuaylas', 'Ahuas', '28/6/2019', 13, 124, 'Active', 700, 1000, 92, 32, 5000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('14', 'John F. Kennedy', 'Abilene Regional Airport', '30/6/2019', 14, 156, 'Active', 500, 0, 156, 0, 3000)
+INSERT INTO FLIGHT ('flight_id','depart_ap',"arrival_ap",'depart_date','plane_id',"capacity",'status','normal_price','fc_price','seats_left','fc_seats_left','miles_price') VALUES ('15', 'Aguadilla Borinquen', 'Samana International', '30/6/2019', 15, 100, 'Active', 500, 0, 100, 0, 3000)
